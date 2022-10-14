77590c0ec64917f81634e57550f7c432904a0d2d
kurumi-ikeda <65322807+mirai110@users.noreply.github.com>
2022-10-14 16:01:26 +0900

    ChoiceChipを使ったタグ選択機能の完成

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_vantan_tame_a/component/app_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,8 +6,66 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CommonAppBar(),
+    return Scaffold(
+      appBar: const CommonAppBar(),
+      body: Column(children: const [
+        FavoriteTag(),
+      ]),
+    );
+  }
+}
+
+class FavoriteTag extends StatefulWidget {
+  const FavoriteTag({Key? key}) : super(key: key);
+
+  @override
+  State<FavoriteTag> createState() => _FavoriteTagState();
+}
+
+class _FavoriteTagState extends State<FavoriteTag> {
+  final tags = [
+    'すべて',
+    'ゲーム',
+    'ミックス',
+    '音楽',
+    'ライブ',
+    'シュミレーション',
+    'アクション&アドベンチャーゲーム',
+    '料理',
+    'スポーツ',
+    '最近アップロードされた動画',
+    '視聴済み',
+    '投稿',
+  ];
+  int _choiceIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 50,
+      color: Colors.black,
+      child: ListView.builder(
+        //横並びにする
+        scrollDirection: Axis.horizontal,
+        itemCount: tags.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            padding: const EdgeInsets.all(5),
+            child: ChoiceChip(
+              label: Text(tags[index]),
+              selected: _choiceIndex == index,
+              backgroundColor: Colors.grey[600],
+              selectedColor: Colors.white,
+              onSelected: (_) {
+                setState(() {
+                  _choiceIndex = index;
+                });
+              },
+            ),
+          );
+        },
+      ),
     );
   }
 }
