b47328c798c93a20b59d28130de084c42aca8354
kurumi-ikeda <65322807+mirai110@users.noreply.github.com>
2022-10-28 14:05:23 +0900

    VideoThumbnailの完成

--- lib/component/video_thumbnail.dart
+++ lib/component/video_thumbnail.dart
@@ -1,50 +1,110 @@
 import 'package:flutter/material.dart';
 
 class VideoThumbnail extends StatelessWidget {
-  const VideoThumbnail({super.key});
+  const VideoThumbnail(
+      {super.key,
+      required this.title,
+      required this.views,
+      required this.channelName,
+      required this.postTime,
+      required this.thumbnailImage,
+      required this.posterImage});
+
+  ///動画タイトル
+  final String title;
+
+  ///再生数
+  final String views;
+
+  ///チャンネル名
+  final String channelName;
+
+  ///投稿時間
+  final String postTime;
+
+  ///サムネイルの画像
+  final String thumbnailImage;
+
+  ///投稿者のアイコンの画像
+  final String posterImage;
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        Container(
-          // padding: const EdgeInsets.all(10),
-          height: 200,
-          width: screenSize.width,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                  'https://i.ytimg.com/vi/miaLdDs4VZ4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAYoeNivBf83iGdjRFH8H0ep-6PbQ'),
-              fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {},
+      child: Column(
+        children: [
+          //サムネイルの部分
+          Container(
+            height: 200,
+            width: screenSize.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  thumbnailImage,
+                ),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        Container(
-          height: 75,
-          width: screenSize.width,
-          color: Colors.black,
-          child: Row(children: [
-            Container(
-              padding: const EdgeInsets.only(
-                bottom: 30,
-              ),
-              child: IconButton(
-                icon: Container(
-                  decoration: const BoxDecoration(
+          //下のテキスト
+          Container(
+            height: 75,
+            width: screenSize.width,
+            color: Colors.black,
+            child: Container(
+              margin: const EdgeInsets.only(top: 5),
+              child:
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                IconButton(
+                  icon: Container(
+                    decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       image: DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage("assets/icon.jpeg"))),
+                        fit: BoxFit.fill,
+                        image: AssetImage(
+                          posterImage,
+                        ),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => {},
                 ),
-                onPressed: () => {},
-              ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text(
+                      "$channelName ･ $views 回視聴 ･ $postTime",
+                      style: const TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 50),
+                    child: IconButton(
+                      alignment: Alignment.topRight,
+                      onPressed: () {},
+                      color: Colors.grey,
+                      icon: const Icon(Icons.more_vert),
+                      iconSize: 15,
+                    ),
+                  ),
+                ),
+              ]),
             ),
-
-            // Text(data)
-          ]),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../component/common_app_bar.dart';
 import '../component/favorite_tag.dart';
+import '../component/video_thumbnail.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -12,10 +13,16 @@ class HomePage extends StatelessWidget {
       appBar: const CommonAppBar(),
       body: Column(children: const [
         FavoriteTag(),
+        SingleChildScrollView(
+            child: VideoThumbnail(
+          channelName: 'チャンネル1',
+          postTime: '10時間前',
+          title: '動画名',
+          views: '100万',
+          posterImage: 'assets/irastoya/car.jpg',
+          thumbnailImage: 'assets/irastoya/焼き芋.jpg',
+        )),
       ]),
     );
   }
 }
-
-
-
